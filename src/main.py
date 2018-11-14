10f6b8b20df6e17981c7caf5c3f56f8b14ae8be7
ahzam1 <ahzam1@ualberta.ca>
2018-11-14 03:31:29 -0700

    hovers,drags

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import time
 import tkinter as tk
+import Pmw
 
 from tkinter import *
 ########### API Functions - data fetching from api.warframestat.us
@@ -44,56 +45,61 @@ def alerts():
     data =response.json()
     #mission, eta
     retarray=[]
+
     for x in data:
         temp =[]
         #strip away information we dont want
         missiondict = x["mission"]
-        selectedkeys=[]
-        for (key, value) in missiondict.items():
-            if key != "reward" and key != "nightmare":
-                selectedkeys.append(key)
-        for key in selectedkeys:
-            if key in missiondict:
-                del missiondict[key]
-        #mission dict now only has the rewards and the nightmare boolean
+
         if "Blueprint" in missiondict["reward"]["asString"]:
             missiondict["reward"]["asString"] = missiondict["reward"]["asString"].replace("Blueprint", "BP")
             # shorten the blueprint word to BP to save space
         temp.append(missiondict["reward"]["asString"])
         temp.append(x["eta"].rsplit(' ', 1)[0]) #time left in this alert
         temp.append(missiondict["nightmare"])
+        temp.append(missiondict["node"])
+        temp.append(missiondict["type"])
+        temp.append(missiondict["minEnemyLevel"])
+        temp.append(missiondict["maxEnemyLevel"])
         retarray.append(temp)
     return retarray
 ###########
 
 ### TKINTER THINGS
-class App():
+class App(tk.Tk):
     def __init__(self):
+
         self.root = tk.Tk()
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
-
-        self.x=screen_width - 280
-        self.y= 25
+        self._offsetx = 0
+        self._offsety = 0
+        self.x= screen_width/2
+        self.y= screen_height/2
         self.root.geometry('+%d+%d' % (self.x, self.y))
         self.root.propagate(1)
         self.root.overrideredirect(1) #set window borderless
         self.root.configure(background="#2b2d31")
+        self.locked= False
 
 
         ##frames for arrangement
         topframe = Frame(self.root, bg="#2b2d31", height=5, bd=1)
         topframe.pack(side=RIGHT, expand= FALSE, fill=Y)
         ephoto = tk.PhotoImage(file="img/exit.png")
+        self.lphoto= tk.PhotoImage(file="img/lock.png")
+        self.uphoto= tk.PhotoImage(file="img/unlock.png")
         exitbutton = Button(topframe, image=ephoto, pady=5, fg="black", bg= "#2b2d31", command=self.destroy_window, relief=FLAT)
         exitbutton.pack(side=TOP)
-        sphoto= tk.PhotoImage(file="img/set.png")
-        settingsbutton = Button(topframe, image=sphoto, pady=5, fg="black", bg= "#2b2d31", command=self.opensettings, relief=FLAT)
-        settingsbutton.pack(side=TOP)
+        Pmw.Balloon(self.root).bind(exitbutton, "Exit")
 
+        self.lockbutton = Button(topframe, image=self.uphoto, pady=5, fg="black", bg= "#2b2d31", command=self.lock, relief=FLAT)
+        self.lockbutton.pack(side=TOP)
+        Pmw.Balloon(self.root).bind(exitbutton, "Lock/Unlock")
+        topframe.bind('<ButtonPress-1>', self.clickwin)
+        topframe.bind('<B1-Motion>', self.dragwin)
         tframe=Frame(self.root, bg="#2b2d31")
         tframe.pack(fill=X)
-
         mainframe=Frame(self.root, bg="#36393e")
         mainframe.pack(fill=X)
         cframe=Frame(mainframe, bg="#36393e", padx=10)
@@ -101,7 +107,7 @@ class App():
         atframe = Frame(mainframe, bg="#2b2d31")
         atframe.pack(fill=X)
         self.aframe=Frame(mainframe, bg="#36393e")
-        self.aframe.pack()
+        self.aframe.pack(fill=X)
         btframe = Frame(mainframe, bg="#2b2d31")
         btframe.pack(fill=X)
         self.bframe = Frame(mainframe, bg="#36393e")
@@ -168,9 +174,14 @@ class App():
                 cleft = "yellow"
             else:
                 cleft = "#49FF00"
-            tk.Label(self.aframe, text=tleft, bg="#36393e", fg=cleft).grid(row=i, column=0, sticky="W")
+
+            reward=tk.Label(self.aframe, text=tleft, bg="#36393e", fg=cleft)
+            reward.grid(row=i, column=0, sticky="W", padx=5)
             tright = "Time: " + alertdata[i][1]
-            tk.Label(self.aframe, text=tright, bg="#36393e", fg="white").grid(row=i, column=1, sticky="E")
+            timer=tk.Label(self.aframe, text=tright, bg="#36393e", fg="white")
+            timer.grid(row=i, column=1, sticky="E", padx=5)
+            Pmw.Balloon(self.root).bind(reward, alertdata[i][3] + " | " +alertdata[i][4] + " | Level: " + str(alertdata[i][5]) +"-"+ str(alertdata[i][6]))
+            Pmw.Balloon(self.root).bind(timer, alertdata[i][3] + " | " +alertdata[i][4]+ " | Level: " + str(alertdata[i][5]) +"-"+ str(alertdata[i][6]))
 
         if not barodata[0]: ##baro not here :(
             temp = "Arriving in " + barodata[1] + " at " + barodata[2]
@@ -182,16 +193,12 @@ class App():
         self.root.after(60000, self.update_clock) #update every minute
     def destroy_window(self):
         self.root.destroy()
-    def opensettings(self):
-        pospop = tk.Tk()
-        pospop.wm_title("Enter position")
-        tk.Label(pospop, text="X =").pack(side=LEFT)
-        xentry= Entry(pospop)
-        xentry.pack(side= LEFT)
-        tk.Label(pospop, text="Y =").pack(side=LEFT)
-        yentry= Entry(pospop)
-        yentry.pack(side= LEFT)
-        Button(pospop,text="Set", padx=2, command= lambda: self.updatepos(xentry, yentry, pospop)).pack()
+    def lock(self):
+        self.locked = not self.locked
+        if self.locked:
+            self.lockbutton.configure(image=self.lphoto)
+        else:
+            self.lockbutton.configure(image=self.uphoto)
     def updatepos(self, x, y, win):
         if x.get().isdigit() and y.get().isdigit():
             self.x=int(x.get())
@@ -201,5 +208,15 @@ class App():
             self.update_clock()
         else:
             tk.Label(win, text="Invalid entry", fg="red").pack(side=LEFT)
+
+    def dragwin(self,event):
+        x = self.root.winfo_pointerx() - self._offsetx
+        y = self.root.winfo_pointery() - self._offsety
+        if not self.locked:
+            self.root.geometry('+{x}+{y}'.format(x=x,y=y))
+
+    def clickwin(self,event):
+        self._offsetx = event.x + self.root.winfo_width() -10
+        self._offsety = event.y
 app=App()
 #tkinter things
